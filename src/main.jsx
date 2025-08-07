c72cac6c836e280b793a1b15dec08350632ba298
Jon <jonskjeseth@yahoo.no>
2025-08-07 12:00:49 +0200

    Bytt til HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css"; // Tailwind-importer
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/framerate/">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
